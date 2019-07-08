bbe769ddb743a4ddfcdf87b90e64ce344e31004d
Nishant Sharma <nishant5788@gmail.com>
2019-07-08 22:41:56 +0530

    Static version implemented with routing. All Basics Static stuffs as of now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'store', component: StoreComponent},
+  { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { BodyContentComponent } from './layout/body-content/body-content.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductEditComponent } from './pages/products/product-edit/product-edit.component';
+import { ProductListComponent } from './pages/products/product-list/product-list.component';
+import { ProductSingleComponent } from './pages/products/product-single/product-single.component';
+import { SiteNavigationComponent } from './common/site-navigation/site-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyContentComponent,
+    AboutComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    StoreComponent,
+    ProductsComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductSingleComponent,
+    SiteNavigationComponent,
   ],
   imports: [
     BrowserModule,
