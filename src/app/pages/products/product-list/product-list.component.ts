8fd7ca9ddb65b8500647f433cdd175b176242ebf
Nishant Sharma <nishant5788@gmail.com>
2019-08-17 21:45:05 +0530

    Pagination Added. Although Shaky. Have some issues like last element is not showing. Other problem is related to product editing. I have added pagination only for non-authorized users only. Will fix these soon after some Reasearch

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -13,17 +13,58 @@ export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
   subscription: Subscription;
   @Input() authStatusFromProduct: boolean;
+  page: number = 1;
+  pageSize: number = this.page + 3;
+  collectionSize: number;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.subscription = this.productService.ProductChanged.subscribe(
-      (newProduct: Product[]) => {
-        this.products = newProduct;
+    // this.subscription = this.productService.ProductChanged.subscribe(
+    //   (newProduct: Product[]) => {
+    //     // this.products = newProduct;
+    //     this.collectionSize = newProduct.length;
+    //     const productSet = newProduct.slice(0,this.pageSize);
+    //     this.products = productSet;
+    //   }
+    // );
+    // this.products = this.productService.getProducts();
+
+    if(!this.authStatusFromProduct) {
+    this.onPaginate();
+    }
+    else {
+    this.productService.ProductChanged.subscribe(
+      res => {
+        this.products = res;
       }
     );
-    this.products = this.productService.getProducts();
+  }
+  }
 
+  onPaginate() {
+    let newPageSize = this.page * 3;
+    let newPage = (this.page - 1) * 3;
+    if(newPage < 0) {
+      newPage = 0;
+    }
+    this.subscription = this.productService.fetchProducts().subscribe(
+      (newProduct: Product[]) => {
+        this.collectionSize = newProduct.length;
+        const productSet = newProduct.slice(newPage, newPageSize);
+        this.products = productSet;
+      }
+    );
+
+    window.scrollTo({
+      top: 150,
+      left: 100,
+      behavior: 'smooth'
+    });
+
+    // console.log("page is " + newPage);
+    // console.log("pagesize is " + newPageSize);
+    
   }
 
   ngOnDestroy() {
